39a27fb36d5dec2a03b3312bf663ad4362dc62f2
Iulian Dragos <dragos@dragos-mbp.local>
2009-11-04 15:39:28 +0100

    Revert "Switched to type parameters for @specialized"
    This reverts commit ee30bc3f61e63155ce5eaa025f352b4370fea0fc.

--- src/compiler/scala/tools/nsc/transform/SpecializeTypes.scala
+++ src/compiler/scala/tools/nsc/transform/SpecializeTypes.scala
@@ -236,45 +236,54 @@ abstract class SpecializeTypes extends InfoTransform with TypingTransformers {
     }
   }
 
-  lazy val primitiveTypes = Set(
-    definitions.UnitClass,
-    definitions.BooleanClass,
-    definitions.ByteClass,
-    definitions.ShortClass,
-    definitions.CharClass,
-    definitions.IntClass,
-    definitions.LongClass,
-    definitions.FloatClass,
-    definitions.DoubleClass)
+  lazy val primitiveTypes = Map(
+    "Unit"    -> definitions.UnitClass.tpe,
+    "Boolean" -> definitions.BooleanClass.tpe,
+    "Byte"    -> definitions.ByteClass.tpe,
+    "Short"   -> definitions.ShortClass.tpe,
+    "Char"    -> definitions.CharClass.tpe,
+    "Int"     -> definitions.IntClass.tpe,
+    "Long"    -> definitions.LongClass.tpe,
+    "Float"   -> definitions.FloatClass.tpe,
+    "Double"  -> definitions.DoubleClass.tpe)
 
   
-  lazy val OrTypeSymbol = definitions.getMember(definitions.ScalaPackage, newTermName("|"))
+
+  /** Parse the given string into the list of types it contains.
+   * 
+   *  @param str comma-separated string of distinct primitive types.
+   */
+  def parseTypes(str: String): List[Type] = {
+    if (str.trim == "")
+      List()
+    else {
+      val buf = new mutable.ListBuffer[Type]
+      for (t <- str.split(',')) 
+        primitiveTypes.get(t.trim) match {
+          case Some(tpe) => buf += tpe
+          case None => 
+            error("Invalid type " + t + ". Expected one of " + primitiveTypes.keysIterator.mkString("", ", ", "."))
+        }
+      buf.toList
+    }
+  }
 
   /** Return the concrete types `sym' should be specialized at.
    */
-  def concreteTypes(sym: Symbol): List[Type] = {
-    println(definitions.ScalaPackage.info.members)
-    println(definitions.ScalaPackage.info.member(newTypeName("|")))
-    assert(OrTypeSymbol != NoSymbol)
-    def flattenType(tp: Type): List[Type] = tp match {
-      case TypeRef(_, sym, args) if (sym == OrTypeSymbol) =>
-        flattenType(args(0)) ::: flattenType(args(1))
-      case TypeRef(_, sym, _) if primitiveTypes.contains(sym) =>
-        List(tp)
-      case TypeRef(_, definitions.NothingClass, _) =>
-        primitiveTypes.toList.map(_.tpe) // default is all primitive types
-      case _ =>
-        error("Invalid type " + tp + ". Expected one of " + primitiveTypes.mkString("", ", ", "."))
-        List()
-    }
+  def concreteTypes(sym: Symbol): List[Type] = 
     sym.getAnnotation(SpecializedClass) match {
-      case Some(AnnotationInfo(tpe, args, _)) =>
-        val tpes = flattenType(tpe).removeDuplicates
-        log(sym + " specialized on " + tpes)
-        tpes
+      case Some(AnnotationInfo(_, args, _)) => 
+        args match {
+          case Literal(ct) :: _ => 
+            val tpes = parseTypes(ct.stringValue)
+            log(sym + " specialized on " + tpes)
+            tpes
+          case _ =>
+            log(sym + " specialized on everything")
+            primitiveTypes.valuesIterator.toList
+        }
       case _ =>
-          Nil
-      }
+        Nil
     }
 
   /** Return a list of all type environements for all specializations 
--- src/library/scala/specialized.scala
+++ src/library/scala/specialized.scala
@@ -13,18 +13,25 @@ package scala
 
 /** Annotate type parameters on which code should be automatically
  *  specialized. For example:
- *  {{{
+ *  <code>
  *    class MyList[@specialized T] ...
- *  }}}
+ *  </code>
  *
  *  Type T can be specialized on a subset of the primitive types by
- *  specifying a number of types separated by |:
+ *  specifying a comma-separated string argument:
  *
- *  {{{
- *   class MyList[@specialized[Int|Double|Boolean] T] ..
- *  }}}
+ *  <code>
+ *   class MyList[@specialized("Int, Double, Boolean") T] ..
+ *  </code>
+ *  Only primitive types are supported and no name resolution is currently
+ *  done on the string arguments (meaning imports and type aliases are
+ *  not resolved).
  *
  *  @since 2.8
  */
-class specialized[T <: AnyVal] extends StaticAnnotation
+class specialized(types: String) extends StaticAnnotation {
+  def this() {
+    this("Boolean, Byte, Short, Char, Int, Long, Float, Double")
+  }
+}
 
