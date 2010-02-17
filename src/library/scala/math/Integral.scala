f3546172c2656911af9af189b8ad269e67c66f80
Iulian Dragos <iulian.dragos@epfl.ch>
2010-02-17 15:54:34 +0100

    Trying to specialize the Numeric hierarchy

--- src/library/scala/math/Integral.scala
+++ src/library/scala/math/Integral.scala
@@ -13,14 +13,22 @@ package scala.math
 /**
  * @since 2.8
  */
-trait Integral[T] extends Numeric[T] {
+trait Integral[@specialized(Int) T] extends Numeric[T] {
   def quot(x: T, y: T): T
   def rem(x: T, y: T): T
   
-  class IntegralOps(lhs: T) extends Ops(lhs) {
-    def /(rhs: T) = quot(lhs, rhs)
-    def %(rhs: T) = rem(lhs, rhs)
-    def /%(rhs: T) = (quot(lhs, rhs), rem(lhs, rhs))
+  override implicit def mkNumericOps(lhs1: T): IntegralOps[T] = new IntegralOps[T] {
+    val lhs = lhs1
+    val n = Integral.this
   }
-  override implicit def mkNumericOps(lhs: T): IntegralOps = new IntegralOps(lhs)
 }
+
+trait IntegralOps[@specialized(Int) T] extends NumericOps[T] {
+  override val n: Integral[T]
+  import n._
+
+  def /(rhs: T) = quot(lhs, rhs)
+  def %(rhs: T) = rem(lhs, rhs)
+  def /%(rhs: T) = (quot(lhs, rhs), rem(lhs, rhs))
+}
+  
