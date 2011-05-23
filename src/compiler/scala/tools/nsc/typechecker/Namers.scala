dragos/scala-before-git

commit 00d175ed5b582a3ea55f93d7b4f67e72762a9144

Modification to the widening logic to treat locally defined symbols like final members thus allowing more constants to be inlined.  Concretely, that means that in code like this:
  def f: Unit = { val b = false ; if (b) println("ok") }

The call to println is no longer generated at all, and in this code:

  def f(x: Int) = { val X = 1 ; val Y = 2; x match { case X => 1 ; case Y => 2 } }

A tableswitch is generated instead of the present if/then/else.

I also added a big comment to the former widenIfNotFinal (now
widenIfNecessary for obvious reasons.) Review by rytz.

git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@23262 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -577,9 +577,27 @@ trait Namers { self: Analyzer =>
       sym.setInfo(selftpe)
     }
 
-    private def widenIfNotFinal(sym: Symbol, tpe: Type, pt: Type): Type = {
+    /** This method has a big impact on the eventual compiled code.
+     *  At this point many values have the most specific possible
+     *  type (e.g. in val x = 42, x's type is Int(42), not Int) but
+     *  most need to be widened to avoid undesirable propagation of
+     *  those singleton types.
+     *
+     *  However, the compilation of pattern matches into switch
+     *  statements depends on constant folding, which will only take
+     *  place for those values which aren't widened.  The "final"
+     *  modifier is the present means of signaling that a constant
+     *  value should not be widened, so it has a use even in situations
+     *  whether it is otherwise redundant (such as in a singleton.)
+     *  Locally defined symbols are also excluded from widening.
+     *
+     *  PP:is there a useful difference in practice between widen
+     *  and deconst which wouldn't be achieved by simply calling widen
+     *  in both situations? If so, could an example of this be added?
+     */
+    private def widenIfNecessary(sym: Symbol, tpe: Type, pt: Type): Type = {
       val getter = 
-        if (sym.isValue && sym.owner.isClass && (sym hasFlag PRIVATE))
+        if (sym.isValue && sym.owner.isClass && sym.isPrivate)
           sym.getter(sym.owner) 
         else sym
       def isHidden(tp: Type): Boolean = tp match {
@@ -597,8 +615,8 @@ trait Namers { self: Analyzer =>
       if ((sym.isVariable || sym.isMethod && !(sym hasFlag ACCESSOR))) 
         if (tpe2 <:< pt) tpe2 else tpe1
       else if (isHidden(tpe)) tpe2
-      else if (!(sym hasFlag FINAL)) tpe1
-      else tpe
+      else if (sym.isFinal || sym.isLocal) tpe
+      else tpe1
     }
 
     // sets each ValDef's symbol
@@ -895,7 +913,7 @@ trait Namers { self: Analyzer =>
           // replace deSkolemized symbols with skolemized ones (for resultPt computed by looking at overridden symbol, right?)
           val pt = resultPt.substSym(tparamSyms, tparams map (_.symbol))
           // compute result type from rhs
-          tpt defineType widenIfNotFinal(meth, typer.computeType(rhs, pt), pt)
+          tpt defineType widenIfNecessary(meth, typer.computeType(rhs, pt), pt)
           tpt setPos meth.pos.focus
           tpt.tpe
         } else typer.typedType(tpt).tpe
@@ -1138,7 +1156,7 @@ trait Namers { self: Analyzer =>
                   context.error(tpt.pos, "missing parameter type");
                   ErrorType
                 } else { 
-                  tpt defineType widenIfNotFinal(
+                  tpt defineType widenIfNecessary(
                     sym, 
                     newTyper(typer1.context.make(vdef, sym)).computeType(rhs, WildcardType), 
                     WildcardType)

test/files/pos/no-widen-locals.scala
@@ -1,0 +1,17 @@
+import annotation.switch
+
+object Test {
+  def f(x: Int) = {
+    val X1 = 5
+    val X2 = 10
+    val X3 = 15
+    val X4 = 20
+    
+    (x: @switch) match {
+      case X1 => 1
+      case X2 => 2
+      case X3 => 3
+      case X4 => 4
+    }
+  }
+}

test/files/run/constrained-types.check
@@ -92,8 +92,8 @@ def m = {
   val x = "three"
   val y : String @Annot(x) = x
   y
-} // x should be existentially bound
-m: java.lang.String @Annot(x) forSome { val x: java.lang.String }
+} // x should not escape the local scope with a narrow type
+m: String @Annot("three")
 
 -----
 def n(y: String) = {

test/files/run/constrained-types.scala
@@ -61,7 +61,7 @@ object Test {
       |  val x = "three"
       |  val y : String @Annot(x) = x
       |  y
-      |} // x should be existentially bound""",
+      |} // x should not escape the local scope with a narrow type""",
 
     """def n(y: String) = {
       |  def m(x: String) : String @Annot(x) = {

commit a6ea138c39701dc3e9901c9651e727a58d995510

Fix for failing { val x = classOf[List[_]] } introduced by paulp in r23262.  I already had this in mind (see comments of #4419) but I was holding back in the runup to 2.9.  Closes #4419, review by odersky.
git-svn-id: https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@25015 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -614,7 +614,7 @@ trait Namers { self: Analyzer =>
      *  modifier is the present means of signaling that a constant
      *  value should not be widened, so it has a use even in situations
      *  whether it is otherwise redundant (such as in a singleton.)
-     *  Locally defined symbols are also excluded from widening.
+     *  Method-defined locals are also excluded from widening.
      */
     private def widenIfNecessary(sym: Symbol, tpe: Type, pt: Type): Type = {
       val getter = 
@@ -636,7 +636,7 @@ trait Namers { self: Analyzer =>
       if ((sym.isVariable || sym.isMethod && !sym.hasAccessorFlag))
         if (tpe2 <:< pt) tpe2 else tpe1
       else if (isHidden(tpe)) tpe2
-      else if (sym.isFinal || sym.isLocal) tpe
+      else if (sym.isFinal || sym.owner.isMethod) tpe
       else tpe1
     }
 

test/files/neg/bug4419.check
@@ -1,0 +1,4 @@
+bug4419.scala:2: error: forward reference extends over definition of value b
+  { val b = a; val a = 1 }
+            ^
+one error found

test/files/neg/bug4419.scala
@@ -1,0 +1,3 @@
+class A {
+  { val b = a; val a = 1 }
+}

test/files/pos/widen-existential.scala
@@ -1,0 +1,3 @@
+class A {
+  { val x = classOf[List[_]] }
+}